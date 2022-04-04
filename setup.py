8c3929ddc4c19fe963433ea8ad5a1691951ef631
cisar2218 <cisar2218@gmail.com>
2022-04-04 22:53:49 +0200

    files reloaded

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='thumby',
+    packages=find_packages(include=['thumby']),
+    version='0.1.0',
+    description='Simple python library for inserting .png thumbnails into gcode files.',
+    author='Dusan Jansky',
+    license='MIT',
+    install_requires=['Pillow>=9.0.1'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
+
+# TODO customise requirments
