79af66f6416b6e05dad83626dcb5d7cee182930c
cisar2218 <cisar2218@gmail.com>
2022-04-11 15:26:06 +0200

    code review, minor changes

--- executable_scripts/gcode_img_inserter.py
+++ executable_scripts/gcode_img_inserter.py
@@ -1,25 +1,22 @@
-from os import fpathconf
 import sys
-from click import option
 import thumby as tb
 
 
 def print_help(err_msg=''):
-    if len(err_msg) > 0:
-        print("Error:", err_msg)
-
     def bold(text):
         return '\033[1m'+text+'\033[0m'
+
+    if len(err_msg) > 0:
+        print(bold("Error:"), err_msg)
     
-    print(bold("usage:"), sys.argv[0], "OPTION", "FILE1", "[FILE2]")
-    print("OPTIONS:")
-    print(bold("inserts"),"- png file to gcode file:")
-    print(bold("FILE1=png_file, FILE2=gcode_file"))
+    print(bold("Usage:"), sys.argv[0], "OPTION", "FILE1", "[FILE2]")
+    print(bold("Options:"))
+    print(bold("Insert"),"- FILE1=png_file, FILE2=gcode_file - png file to gcode file:")
     print("--insert-all\t-iall\tFILE1\tFILE2\tall size (recommanded)")
     print("--insert-mini\t-imini\tFILE1\tFILE2\tmini size\t"+str(tb.WIDTH_MINI)+"x"+str(tb.HEIGHT_MINI))
     print("--insert-normal\t-inorm\tFILE1\tFILE2\tnormal size\t"+str(tb.WIDTH_NORMAL)+"x"+str(tb.HEIGHT_NORMAL))
     print("--insert-large\t-ilarge\tFILE1\tFILE2\tlarge size\t"+str(tb.WIDTH_LARGE)+"x"+str(tb.HEIGHT_LARGE))
-    print(bold("clear"),"- FILE1=gcode_file - delete current thumbnails from gcode")
+    print(bold("Clear"),"- FILE1=gcode_file - delete current thumbnails from gcode")
     print("--clear\t\t-c\tFILE1\t(None)\tdeletes thumbnails from gcode file")
 
     exit()
@@ -49,14 +46,14 @@ def insert_gcode(opt, fpath_png, fpath_gcode):
 
 if __name__=='__main__':
     if len(sys.argv) < 2:
-        print_help("no arguments")
+        print_help()
 
     opt = sys.argv[1]
 
     # clear_code
     if len(sys.argv) >= 3 and (opt == '--clear' or opt == '-c'):
         fpath_gcode = sys.argv[2]
-        if not fpathconf.lower().endswith('.gcode'):
+        if not fpath_gcode.lower().endswith('.gcode'):
             print_help("invalid filetype given " + fpath_gcode)
         clear_gcode(fpath_gcode)
     # insert gcode
@@ -64,12 +61,9 @@ if __name__=='__main__':
         fpath_png = sys.argv[2]
         fpath_gcode = sys.argv[3]
 
-        if not fpathconf.lower().endswith('.gcode') or not fpath_png.lower().endswith('.png'):
+        if not fpath_gcode.lower().endswith('.gcode') or not fpath_png.lower().endswith('.png'):
             print_help("check format of given files: " + fpath_png + " " + fpath_gcode)
         
         insert_gcode(opt, fpath_png, fpath_gcode)
     else:
         print_help("wrong format of arguments: '" + opt+"'")
-
-
-    
